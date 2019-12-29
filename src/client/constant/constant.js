835357067819e61f51cfd474ce718c68bb5377e0
anhtran226 <theanh0226@gmail.com>
2019-12-29 11:44:55 +0100

    add feature tutor chat room

--- src/client/constant/constant.js
+++ src/client/constant/constant.js
@@ -1,5 +1,6 @@
 export const SERVER_BACKEND = 'http://localhost:5000';
 export const END_POINT_SOCKET = 'http://localhost:5000';
+export const TUTOR_ROOM = 31122019;
 // CHANGE PORT IP
 // export const SERVER_BACKEND = 'http://192.268.2.101:5000';
 // export const END_POINT_SOCKET = 'http://192.268.2.101:5000';
